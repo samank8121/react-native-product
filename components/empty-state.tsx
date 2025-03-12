bd6204d4b0935a85f32fe041e0e7e2b72d641129
saman <saman.k8121@gmail.com>
2025-03-12 16:01:31 +0300

    refactor: remove unused font

--- components/empty-state.tsx
+++ components/empty-state.tsx
@@ -7,7 +7,7 @@ const EmptyState = ({ title }: { title: string }) => {
     <View className='flex justify-center items-center px-4'>
       <FontAwesome name='search' size={48} color='white' />
 
-      <Text className='text-sm font-pmedium text-gray-100 mt-5'>{title}</Text>
+      <Text className='text-sm text-gray-100 mt-5'>{title}</Text>
     </View>
   );
 };
